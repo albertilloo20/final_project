2d0a0dd0dd05b7a565c98df7a373d38ea802efee
albertoSierra <sierrajimenezalberto@gmail.com>
2017-12-20 01:43:40 +0100

    Realizar pruebas ngBootstrap

--- AlquilaCar/src/app/componentes/listado-coches/listado-coches.component.ts
+++ AlquilaCar/src/app/componentes/listado-coches/listado-coches.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {CochesService} from "../../services/coches.service";
+import * as firebase from 'firebase';
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-listado-coches',
@@ -6,10 +10,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-coches.component.css']
 })
 export class ListadoCochesComponent implements OnInit {
+  /*coche = {
+    alquilado: false,
+    descripcion: '',
+    id: '',
+    imageurl: '',
+    km: '',
+    marca: '',
+    modelo: '',
+    plazas: '',
+    precio: '',
+    puertas: '',
+    username: ''
+  }*/
+  arrayCoches;
+  username;
+  mensajeOk = '';
+  constructor(private _cochesService: CochesService, private _userService: UserService, public route: Router) {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this._userService.getEmail(user.email).subscribe(resp => {
+          const RESPONSE = resp;
+          const RESULT = [];
+          for (let item in RESPONSE) {
+            RESULT.push(RESPONSE[item]);
+          }
+          this.username = RESULT[0].username;
+          this._cochesService.getCochesByUser(this.username).subscribe(res => {
+            const RESPONSECARS = res;
+            const RESULTCARS = [];
+            for (let item in RESPONSECARS) {
+              RESULTCARS.push(RESPONSECARS[item]);
+            }
+            this.arrayCoches = RESULTCARS;
+            console.log(this.arrayCoches);
+          }, error => {
+            console.log(error);
+          });
+        });
+      } else {
 
-  constructor() { }
+      }
+    });
+  }
 
   ngOnInit() {
   }
+  cambiaAlquilado(id, alquilado){
+    this._cochesService.updateAlquilado(id, alquilado);
+    this._cochesService.getCochesByUser(this.username).subscribe(res => {
+      const RESPONSECARS = res;
+      const RESULTCARS = [];
+      for (let item in RESPONSECARS) {
+        RESULTCARS.push(RESPONSECARS[item]);
+      }
+      this.arrayCoches = RESULTCARS;
+      console.log(this.arrayCoches);
+    }, error => {
+      console.log(error);
+    });
+  }
 
+  borrarCoche(id) {
+    this._cochesService.borrarCoche(id).subscribe(res => {
+      this._cochesService.getCochesByUser(this.username).subscribe(res => {
+        const RESPONSECARS = res;
+        const RESULTCARS = [];
+        for (let item in RESPONSECARS) {
+          RESULTCARS.push(RESPONSECARS[item]);
+        }
+        this.arrayCoches = RESULTCARS;
+        console.log(this.arrayCoches);
+      }, error => {
+        console.log(error);
+      });
+      this.mensajeOk = 'Coche eliminado con exito';
+    });
+
+  }
 }
--- AlquilaCar/src/app/services/coches.service.ts
+++ AlquilaCar/src/app/services/coches.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import * as firebase from 'firebase';
+import {AngularFireDatabase} from "angularfire2/database";
 
 @Injectable()
 export class CochesService {
   cochesURL = 'https://alquilacar-a9f10.firebaseio.com/coches.json';
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, public db: AngularFireDatabase) { }
 
   getCoches() {
     return this.httpClient.get(this.cochesURL).pipe(map((data: any) => {
@@ -39,6 +41,26 @@ export class CochesService {
   getCochesByAlquilado(alquilado){
     const URLById = this.cochesURL + '?orderBy="alquilado"&equalTo=' + alquilado ;
     return this.httpClient.get(URLById).pipe(map((data: any) => {
+      return data;
+    }));
+  }
+
+  getCochesByUser(username){
+    const URLByUsername = this.cochesURL + '?orderBy="username"&equalTo="' + username + '"';
+    return this.httpClient.get(URLByUsername).pipe(map((data: any) => {
+      return data;
+    }));
+  }
+  updateAlquilado(id, alquilado){
+    let ref = this.db.list('coches/');
+    ref.update(id,{
+      alquilado: alquilado
+    });
+  }
+  borrarCoche(id){
+    const URLById = 'https://alquilacar-a9f10.firebaseio.com/coches/' + id + '.json';
+    return this.httpClient.delete(URLById).pipe(map((data: any) => {
+      console.log(data);
       return data;
     }));
   }
